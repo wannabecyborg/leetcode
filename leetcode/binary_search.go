36005ab3927baaa83eefbd5597f5d46fa360eccf
wbc <wbc@>
2024-03-03 21:57:07 +0500

    bin search

---
+++ leetcode/binary_search.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func search(nums []int, target int) int {
+	low, high := 0, len(nums)-1
+
+	for low <= high {
+		mid := (high + low) / 2
+		if target == nums[mid] {
+			return mid
+		}
+		if target > nums[mid] {
+			low = mid + 1
+		} else if target < nums[mid] {
+			high = mid - 1
+		}
+	}
+	return -1
+}

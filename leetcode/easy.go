5a26d40d7a936233ec596812ad8927389b651e59
wbc <wbc@>
2024-03-09 16:28:10 +0500

    containsNearbyDuplicate

---
+++ leetcode/easy.go
@@ -0,0 +1,18 @@
+package leetcode
+
+type T struct{}
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	hs := make(map[int]T)
+	for i := 0; i < len(nums); i++ {
+		_, isOk := hs[nums[i]]
+		if isOk {
+			return true
+		}
+		hs[nums[i]] = T{}
+		if i >= k {
+			delete(hs, nums[i-k])
+		}
+	}
+	return false
+}
